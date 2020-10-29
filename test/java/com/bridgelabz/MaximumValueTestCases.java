b4c516333231f668a2aa00437b0f9bbc55013e48
ShravyaHubb <shravyak146@gmail.com>
2020-10-29 15:30:02 +0530

    ADDED: code to print maximum value

--- test/java/com/bridgelabz/MaximumValueTestCases.java
+++ test/java/com/bridgelabz/MaximumValueTestCases.java
@@ -7,56 +7,74 @@ public class MaximumValueTestCases {
 
     @Test
     public void givenFirstInteger_WhenMaximum_ShouldReturnFirstInteger() {
-        MaximumValueTest maximumValueTest = new MaximumValueTest();
-        Assert.assertEquals(4, maximumValueTest.maximumValue(4, 3, 2));
+        MaximumValueTest maximumValueTest = new MaximumValueTest(4, 3, 2);
+        Assert.assertEquals(4, maximumValueTest.testMaximum());
     }
 
     @Test
     public void givenSecondInteger_WhenMaximum_ShouldReturnSecondInteger() {
-        MaximumValueTest maximumValueTest = new MaximumValueTest();
-        Assert.assertEquals(3, maximumValueTest.maximumValue(1, 3, 2));
+        MaximumValueTest maximumValueTest = new MaximumValueTest(1, 3, 2);
+        Assert.assertEquals(3, maximumValueTest.testMaximum());
     }
 
     @Test
     public void givenThirdInteger_WhenMaximum_ShouldReturnThirdInteger() {
-        MaximumValueTest maximumValueTest = new MaximumValueTest();
-        Assert.assertEquals(5, maximumValueTest.maximumValue(4, 3, 5));
+        MaximumValueTest maximumValueTest = new MaximumValueTest(4, 3, 5);
+        Assert.assertEquals(5, maximumValueTest.testMaximum());
     }
 
     @Test
-    public void givenFirstDouble_WhenMaximum_ShouldReturnFirstDouble() {
-        MaximumValueTest maximumValueTest = new MaximumValueTest();
-        Assert.assertEquals(4.4, maximumValueTest.maximumValue(4.4, 4.2, 2.0), 0);
+    public void givenFirstFloat_WhenMaximum_ShouldReturnFirstFloat() {
+        MaximumValueTest maximumValueTest = new MaximumValueTest(4f, 3f, 2f);
+        Assert.assertEquals(4f, maximumValueTest.testMaximum());
     }
 
     @Test
-    public void givenSecondDouble_WhenMaximum_ShouldReturnSecondDouble() {
-        MaximumValueTest maximumValueTest = new MaximumValueTest();
-        Assert.assertEquals(3.1, maximumValueTest.maximumValue(1.1, 3.1, 2.1), 0);
+    public void givenSecondFloat_WhenMaximum_ShouldReturnSecondFloat() {
+        MaximumValueTest maximumValueTest = new MaximumValueTest(1f, 3f, 2f);
+        Assert.assertEquals(3f, maximumValueTest.testMaximum());
     }
 
     @Test
-    public void givenThirdDouble_WhenMaximum_ShouldReturnThirdDouble() {
-        MaximumValueTest maximumValueTest = new MaximumValueTest();
-        Assert.assertEquals(4.6, maximumValueTest.maximumValue(4.5, 4.3, 4.6), 0);
+    public void givenThirdFloat_WhenMaximum_ShouldReturnThirdFloat() {
+        MaximumValueTest maximumValueTest = new MaximumValueTest(4f, 3f, 5f);
+        Assert.assertEquals(5f, maximumValueTest.testMaximum());
     }
 
     @Test
     public void givenFirstString_WhenMaximum_ShouldReturnFirstString() {
-        MaximumValueTest maximumValueTest = new MaximumValueTest();
-        Assert.assertEquals("Hi", maximumValueTest.maximumValue("Hi", "Hello", "Hey"));
+        MaximumValueTest maximumValueTest = new MaximumValueTest("Hi", "Hello", "Hey");
+        Assert.assertEquals("Hi", maximumValueTest.testMaximum());
     }
 
     @Test
     public void givenSecondString_WhenMaximum_ShouldReturnSecondString() {
-        MaximumValueTest maximumValueTest = new MaximumValueTest();
-        Assert.assertEquals("Mango", maximumValueTest.maximumValue("Apple", "Mango", "Grape"));
+        MaximumValueTest maximumValueTest = new MaximumValueTest("Apple", "Mango", "Grape");
+        Assert.assertEquals("Mango", maximumValueTest.testMaximum());
     }
 
     @Test
     public void givenThirdString_WhenMaximum_ShouldReturnThirdString() {
+        MaximumValueTest maximumValueTest = new MaximumValueTest("Pink", "Blue", "Red");
+        Assert.assertEquals("Red", maximumValueTest.testMaximum());
+    }
+
+    @Test
+    public void givenMoreThanThreeIntegers_WhenMaximum_ShouldReturnMax() {
         MaximumValueTest maximumValueTest = new MaximumValueTest();
-        Assert.assertEquals("Red", maximumValueTest.maximumValue("Pink", "Blue", "Red"));
+        Assert.assertEquals(7, maximumValueTest.max(4, 2, 5, 7, 6));
+    }
+
+    @Test
+    public void givenMoreThanThreeFloats_WhenMaximum_ShouldReturnMax() {
+        MaximumValueTest maximumValueTest = new MaximumValueTest();
+        Assert.assertEquals(7f, maximumValueTest.max(4f, 2f, 5f, 7f, 6f));
+    }
+
+    @Test
+    public void givenMoreThanThreeStrings_WhenMaximum_ShouldReturnMax() {
+        MaximumValueTest maximumValueTest = new MaximumValueTest();
+        Assert.assertEquals("Red", maximumValueTest.max("Red", "Blue", "Orange", "Pink"));
     }
 
 }
