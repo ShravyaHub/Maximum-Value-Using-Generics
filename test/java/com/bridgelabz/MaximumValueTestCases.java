74be304543857a53519463204e9202004a70417f
ShravyaHubb <shravyak146@gmail.com>
2020-10-29 11:49:24 +0530

    ADDED: code to test maximum of three strings using test cases

--- test/java/com/bridgelabz/MaximumValueTestCases.java
+++ test/java/com/bridgelabz/MaximumValueTestCases.java
@@ -26,19 +26,37 @@ public class MaximumValueTestCases {
     @Test
     public void givenFirstDouble_WhenMaximum_ShouldReturnFirstDouble() {
         MaximumValueTest maximumValueTest = new MaximumValueTest();
-        Assert.assertEquals(4.4, maximumValueTest.maximumNumber(4.4, 4.2, 2.0), 0);
+        Assert.assertEquals(4.4, maximumValueTest.maximumValue(4.4, 4.2, 2.0), 0);
     }
 
     @Test
     public void givenSecondDouble_WhenMaximum_ShouldReturnSecondDouble() {
         MaximumValueTest maximumValueTest = new MaximumValueTest();
-        Assert.assertEquals(3.1, maximumValueTest.maximumNumber(1.1, 3.1, 2.1), 0);
+        Assert.assertEquals(3.1, maximumValueTest.maximumValue(1.1, 3.1, 2.1), 0);
     }
 
     @Test
     public void givenThirdDouble_WhenMaximum_ShouldReturnThirdDouble() {
         MaximumValueTest maximumValueTest = new MaximumValueTest();
-        Assert.assertEquals(4.6, maximumValueTest.maximumNumber(4.5, 4.3, 4.6), 0);
+        Assert.assertEquals(4.6, maximumValueTest.maximumValue(4.5, 4.3, 4.6), 0);
+    }
+
+    @Test
+    public void givenFirstString_WhenMaximum_ShouldReturnFirstString() {
+        MaximumValueTest maximumValueTest = new MaximumValueTest();
+        Assert.assertEquals("Hi", maximumValueTest.maximumValue("Hi", "Hello", "Hey"));
+    }
+
+    @Test
+    public void givenSecondString_WhenMaximum_ShouldReturnSecondString() {
+        MaximumValueTest maximumValueTest = new MaximumValueTest();
+        Assert.assertEquals("Mango", maximumValueTest.maximumValue("Apple", "Mango", "Grape"));
+    }
+
+    @Test
+    public void givenThirdString_WhenMaximum_ShouldReturnThirdString() {
+        MaximumValueTest maximumValueTest = new MaximumValueTest();
+        Assert.assertEquals("Red", maximumValueTest.maximumValue("Pink", "Blue", "Red"));
     }
 
 }
